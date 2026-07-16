75bd73b46401193769027d0eb869d865a960f6e4
arskmc <morkdaniel98@gmail.com>
2026-07-16 20:48:03 +0800

    added transfer feature and refactored a lot of shit
    refactored my ass
    
    TODO: edge cases and make it prettier

--- Budget Tracker/main.py
+++ Budget Tracker/main.py
@@ -1,3 +1,7 @@
+# TODO: make it scalable, start with an empty dict
+# TODO: remove add wallet, unnecessary
+
+import sys
 
 wallets = {
     "Cash": 500,
@@ -11,21 +15,29 @@ expenses = {
     "School": 0
 }
 
-def printBudget():
-    totalBudget = sum(wallets.values())
+def displayWallet():
     for key, value in wallets.items(): 
         print(f"{key}: ₱{value}")
+
+def displayExpenses():
+    for key, value in expenses.items():
+        print(f"{key}: ₱{value}")
+
+def printBudget():
+    totalBudget = sum(wallets.values())
+    
+    displayWallet()
     
     print(f"\nTotal: ₱{totalBudget}")
 
 def printExpenses():
     totalExpenses = sum(expenses.values())
-    for key, value in expenses.items():
-        print(f"{key}: ₱{value}")
+    
+    displayExpenses()
 
     print(f"\nTotal: ₱{totalExpenses}")
 
-def addIncome():
+def chooseWallet():
     walletNames = list(wallets.keys())
     for index, source in enumerate(walletNames):
         print(f"{index + 1}. {source}")
@@ -36,12 +48,9 @@ def addIncome():
         return
     
     selectedWallet = walletNames[choice - 1]
+    return selectedWallet
 
-    amount = float(input("Amount to add: "))
-
-    wallets[selectedWallet] += amount
-
-def addExpense():
+def chooseExpense():
     expenseNames = list(expenses.keys())
 
     for index, source in enumerate(expenseNames):
@@ -53,25 +62,39 @@ def addExpense():
         return
     
     selectedExpense = expenseNames[choice - 1]
+    return selectedExpense
+
+def addIncome():
+    selectedWallet = chooseWallet()
+
+    amount = float(input("Amount to add: "))
+    wallets[selectedWallet] += amount
+
+def addExpense():
+    
+    selectedExpense = chooseExpense()
+   
     amount = float(input("Amount to add: "))
     expenses[selectedExpense] += amount ## ^ adding amount to expense category
 
-    walletNames = list(wallets.keys())
-    for index, source in enumerate(walletNames):
-        print(f"{index + 1}. {source}")
-
-    choice = int(input("Choose a wallet to deduct from: "))
-    if choice <= 0 or choice > len(walletNames):
-        print("Invalid option")
-        return
-    
-    selectedWallet = walletNames[choice - 1]
+    selectedWallet = chooseWallet()
     deduction = float(input("Amount to deduct: "))
 
     wallets[selectedWallet] -= deduction
 
-def main():
-    choice = 0
+def transferMoney():
+    print(f"Transfer from: ")
+    selectedWallet = chooseWallet()
+    print(f"Transfer to: ")
+    receivingWallet = chooseWallet()
+    amount = float(input("Amount to transfer: "))
+
+    wallets[selectedWallet] -= amount
+    wallets[receivingWallet] += amount
+
+    #edge cases: transferring to same wallet, amount more than balance
+
+def dashboard():
     print("==================") 
     print("Budget Tracker\n")
     print("==================\n")
@@ -82,19 +105,36 @@ def main():
     print(f"\nThis Month:")
     printExpenses()
 
+    print("==================\n")
+
+
+def main():
+    choice = 0
+
+    dashboard()
     while True:
         print("What would you like to do?\n")
         print("[1] Add Income") # add value to key
         print("[2] Add Expense") # add expense, subtract from wallet
-        print("[3] Add Wallet")
-        print("[4] Transfer Money")
-        print("[5] View History")
-        print("[6] Exit\n")
+        print("[3] Transfer Money")
+        print("[4] Exit\n")
         choice = int(input("Enter choice: "))
 
         match (choice):
-            case 1: addIncome()
-            case 2: addExpense()
+            case 1: 
+                addIncome()
+                dashboard()
+            case 2: 
+                addExpense()
+                dashboard()
+            case 3: 
+                transferMoney()
+                dashboard()
+            case 4: 
+                print("Exiting...")
+                sys.exit()
+            case _:
+                print("Invalid option.")
     
 main()
 
